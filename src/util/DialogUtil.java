8766c1a4dbc676f746a95d61a43146eaeed90243
Carloz Eduardo Rosa Jacoia <ra2357046>
2025-05-13 08:50:51 -0300

    refatoração IV

--- src/util/DialogUtil.java
+++ src/util/DialogUtil.java
@@ -1,6 +1,7 @@
 
 package util;
 
+import java.util.Optional;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonBar;
@@ -21,6 +22,21 @@ public class DialogUtil {
         alert.setHeaderText(null);
         alert.setContentText(mensagem);
         alert.showAndWait();
+    }
+    
+    public static boolean showConfirmation(String nome, String operacao){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Confirmação de "+operacao);
+        alert.setHeaderText(null);
+        alert.setContentText("Você tem certeza que deseja "+operacao+" o usuário "+nome+"?");
+        
+        ButtonType sim = new ButtonType("Sim", ButtonBar.ButtonData.YES);
+        ButtonType nao = new ButtonType("Não", ButtonBar.ButtonData.NO);
+        
+        alert.getButtonTypes().setAll(sim, nao);
+        Optional<ButtonType> resultado = alert.showAndWait();
+        
+        return resultado.get() == sim;
     }
 }
 
